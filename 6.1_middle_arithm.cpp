23217274fda7d6d32cbd905a14c051e5d4a60620
AliseMarfina <almarfina@gmail.com>
2025-03-02 21:57:32 +0300

    Add files via upload
    Напишите функцию getNumberOfOrderedRows, имеющую три параметра:
    
    адрес матрицы (элементы матрицы-целые числа, матрица в динамической памяти)
    количество строк
    количество столбцов
    Функция должна возвращать количество строк, элементы которых упорядочены по возрастанию.  
    
    Для проверки является ли строка упорядоченной написать функцию isOrderedArray. Функция должна иметь два параметра:
    
    адрес одномерного массива (элементы массива -целые числа)
    количество элементов
    Функция должна возвращать значение true, если элементы массива упорядочены по возрастанию, и false –в противном случае.
    
     Для тестирования функций 6.1:
    
    опишите массив(матрицу) в динамической памяти, элементы матрицы – целые числа;
    введите количество строк и столбцов (количество строк и количество столбцов должны быть заданы целыми положительными числами);
    предусмотрите обработку исключений, если количество строк/столбцов заданы некорректно
    выделите память для матрицы, предусмотрите обработку исключения, если память не выделена;
    введите элементы матрицы (предусмотрите обработку исключений, если элементы заданы некорректно);
    для корректно заданных данных вызовите функции и выведите ответ;
    для ввода элементов матрицы используйте функцию из задания 6.А
     Выводимые сообщения: 
    
    ERROR: Invalid number of rows specified
    
    ERROR: Invalid number of columns specified
    
    ERROR: Memory not allocated for array
    
    ERROR: Invalid matrix element
    
    ERROR: Not enough matrix elements 
    
    Number of ordered rows = < количество строк >

---
+++ 6.1_middle_arithm.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <stdexcept>
+static int arithmeticMeansOfPositive(int* address, int rows, int columns, double* vector);
+{
+    for (int i; i < rows; ++i)
+    {
+        int poscounter = 0;
+        int sum = 0;
+        for (int j = 0; j < columns; ++j)
+        {
+            if (address[i][j] > 0)
+            {
+                sum += address[i][j];
+                poscounter++;
+            }
+        }
+        if (poscounter == 0)
+        {
+            std::cerr << "ERROR: It is impossible to construct a vector" << std::endl;
+        }
+        vector[i] = static_cast<double>(sum) / poscounter;
+    }
+}
+int main()
+{
+    int rows, columns;
+    std::cin >> rows >> columns;
+    if (rows < 0)
+    {
+        std::cerr << "ERROR: Invalid number of rows specified" << std::endl;
+        return 1;
+    }
+    if (columns < 0)
+    {
+        std::cerr << "ERROR: Invalid number of columns specified" << std::endl;
+        return 1;
+    }
+    int** address = new (std::nothrow) int* [rows];
+    if (!address)
+    {
+        std::cerr << "ERROR: Memory not allocated for array" << std::endl;
+        return 1;
+    }
+    for (int i = 0; i < rows; ++i)
+    {
+        address[i] = new(std::nothrow) int[columns];
+        if (!address[i])
+        {
+            std::cerr << "ERROR: Memory not allocated for array" << std::endl;
+            return 1;
+        }
+    }
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j < columns; ++j)
+        {
+            if (!(std::cin >> address[i][j]))
+            {
+                std::cerr << "ERROR: Invalid matrix element" << std::endl;
+                return 1;
+            }
+        }
+    }
+    double* vector = new (std::nothrow) double[rows];
+    if (!vector)
+    {
+        std::cerr << "ERROR: Memory not allocated for array" << std::endl;
+    }
+    std::cout << "Vector of arithmetic means = ";
+    for (int i = 0; i < rows; ++i)
+    {
+        std::cout << vector[i] << " ";
+    }
+    delete[] vector;
+    delete[] address;
+    return 0;
+}
+
+int arithmeticMeansOfPositive(int* address, int rows, int columns, double* vector)
+{
+    return 0;
+}
